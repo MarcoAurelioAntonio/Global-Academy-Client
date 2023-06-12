5a51ee5be629bbf2a55bc28dbfd72073ce413f64
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2023-06-12 15:02:14 -0500

    Add provider and store imports to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Provider store={store}>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </Provider>
   );
 }
