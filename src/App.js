287f1ef5858cf9a4470713dd694c497dcc8d3f9d
Gustavo Porcilio <gustavoporcilio@gmail.com>
2023-06-12 17:47:48 -0300

    Set up router and create a courses component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import Courses from './components/Courses';
 
-export default function App() {
+const router = createBrowserRouter([
+  {
+    path: '/',
+    children: [
+      {
+        path: '/courses',
+        element: <Courses />,
+      },
+    ],
+  },
+]);
+
+function App() {
   return (
-    <Provider store={store}>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </Provider>
+    <main className="main-app">
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </main>
   );
 }
+
+export default App;
---
+++ src/components/Courses.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Courses = () => (
+  <div>
+    <h1>Courses</h1>
+  </div>
+);
+
+export default Courses;
