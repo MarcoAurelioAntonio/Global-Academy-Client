82a2bb20e0f83b76a64b421d939c4045792569bc
Gustavo Porcilio <gustavoporcilio@gmail.com>
2023-06-13 16:26:21 -0300

    Component done and refactor slice

--- src/components/DisplayReservations.jsx
+++ src/components/DisplayReservations.jsx
@@ -1,10 +1,62 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import {
+  useDispatch,
+  useSelector,
+} from 'react-redux';
 import { getAllReservationsApi } from '../redux/displayreservationsSlice';
 
-const DisplayReservations = () => (
-  <div>
-    <h1>DisplayReservations</h1>
-  </div>
-);
+const DisplayReservations = () => {
+  const dispatch = useDispatch();
+  const apiData = useSelector((state) => state.displayreservations.data);
+  const apiStatus = useSelector((state) => state.displayreservations.status);
+  const apiError = useSelector((state) => state.displayreservations.error);
+
+  useEffect(() => {
+    dispatch(getAllReservationsApi());
+  }, [dispatch]);
+
+  return (
+    <div style={{
+      display: 'flex', justifyContent: 'center', alignItems: 'center', height: 'auto',
+    }} // some style to see the content. DETELE IT
+    >
+      <h1>DisplayReservations</h1>
+      <br />
+      {apiStatus === 'loading' && <p>Loading...</p>}
+
+      <ul>
+        {apiData.map((reservation) => (
+          <li key={reservation.id}>
+            <h2>
+              NAME:
+              {reservation.course.name}
+            </h2>
+            <p>
+              DESCRIPTION:
+              {reservation.course.description}
+            </p>
+            <p>
+              Start Date:
+              {reservation.course.start_date}
+            </p>
+            <p>
+              End Date:
+              {reservation.course.end_date}
+            </p>
+            <br />
+            <br />
+          </li>
+        ))}
+      </ul>
+
+      {apiStatus === 'failed' && (
+      <p>
+        Error:
+        {apiError}
+      </p>
+      )}
+    </div>
+  );
+};
 
 export default DisplayReservations;
--- src/redux/displayreservationsSlice.js
+++ src/redux/displayreservationsSlice.js
@@ -2,15 +2,16 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 export const getAllReservationsApi = createAsyncThunk(
-  'apireservations/fetchAllReservations',
+  'api/getAllReservationsApi',
   async () => {
-    const response = await axios.get('http://localhost:3001/reservations');
+    const response = await axios.get('http://localhost:3000/api/v1/users/3/reservations');
+    // console.log(response.data);
     return response.data;
   },
 );
 
 const displayreservationsSlice = createSlice({
-  name: 'apireservations',
+  name: 'api',
   initialState: {
     data: [],
     status: null,
