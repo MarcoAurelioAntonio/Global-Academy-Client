380cd5a7c6b4c9d8b61aa3c389d921318db50620
Gustavo Porcilio <gustavoporcilio@gmail.com>
2023-06-13 14:13:09 -0300

    create displayreservationSlice and install npm install react-redux

--- src/components/DisplayReservations.jsx
+++ src/components/DisplayReservations.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { getAllReservationsApi } from '../redux/displayreservationsSlice';
 
 const DisplayReservations = () => (
   <div>
--- src/redux/displayreservationsSlice.js
+++ src/redux/displayreservationsSlice.js
@@ -1,1 +1,30 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
+export const getAllReservationsApi = createAsyncThunk(
+  'apireservations/fetchAllReservations',
+  async () => {
+    const response = await axios.get('http://localhost:3001/reservations');
+    return response.data;
+  },
+);
+
+const displayreservationsSlice = createSlice({
+  name: 'apireservations',
+  initialState: {
+    data: [],
+    status: null,
+    error: null,
+  },
+
+  reducers: {},
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAllReservationsApi.pending, (state) => ({ ...state, status: 'loading' }))
+      .addCase(getAllReservationsApi.fulfilled, (state, action) => ({ ...state, status: 'succeeded', data: action.payload }))
+      .addCase(getAllReservationsApi.rejected, (state, action) => ({ ...state, status: 'failed', error: action.error.message }));
+  },
+});
+
+export default displayreservationsSlice.reducer;
