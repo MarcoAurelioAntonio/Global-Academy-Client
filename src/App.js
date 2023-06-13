a2be776f853b25c748d642c26be86e9966fe583e
Kender <kenderromain@gmail.com>
2023-06-13 12:08:31 -0400

    Create login form.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import Courses from './components/Courses';
 import Reservations from './components/Reservations';
+import Login from './components/Login';
 
 const router = createBrowserRouter([
   {
     path: '/',
     children: [
+      {
+        path: '/login',
+        element: <Login />,
+      },
       {
         path: '/courses',
         element: <Courses />,
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [inputs, setInputs] = useState({ name: '' });
+  // const dispatch = useDispatch();
 
-export default Login
+  const handleChange = (ev) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [ev.target.name]: ev.target.value,
+    }));
+  };
+  const handleClick = (ev) => {
+    ev.preventDefault();
+    // dispatch(postBookToAPI(inputs));
+  };
+  return (
+    <section className="content">
+      <form onSubmit={handleClick}>
+        <div className="imgcontainer">LOGO HERE</div>
+
+        <div className="container">
+          <label htmlFor="username">
+            <b>Username</b>
+          </label>
+          <input
+            type="text"
+            placeholder="Enter Username"
+            value={inputs.title}
+            id="username"
+            name="name"
+            required
+            onChange={handleChange}
+          />
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default Login;
