1e17cacd03202a3f21dc5e62a49fe721603948ff
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2023-06-14 11:12:35 -0500

    create a nav-menu component and add a home page link

---
+++ src/components/NavMenu.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+const NavMenu = () => (
+  <div className="nav-menu">
+    <div className="nav-menu-item">
+      <Link to="/">Courses</Link>
+    </div>
+  </div>
+);
+
+export default NavMenu;
