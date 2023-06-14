9216d8ffb8d7627a63af4f65ceab79cf1e9829ce
biruk-bereka <birukbeletech1212@gmail.com>
2023-06-14 20:13:57 +0300

    Fix linters error

--- src/components/CourseDetails.jsx
+++ src/components/CourseDetails.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { getCourseById } from "../redux/coursesSlice";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCourseById } from '../redux/coursesSlice';
 
 const CourseDetails = () => {
   const { id } = useParams();
@@ -11,41 +11,61 @@ const CourseDetails = () => {
   useEffect(() => {
     dispatch(getCourseById(id));
   }, [dispatch, id]);
- 
+
   const hanldeEnroll = () => {
-    console.log('enroll')
-  }
+    console.log('enroll');
+  };
 
   return (
     <div className="flex justify-around mt-10 h-screen">
       <div>
         <img
-          width='70%'
-          src='https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80'
+          width="70%"
+          src="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
           alt={course.title}
         />
       </div>
 
       <div>
-        <h1>Course tite: {course.name}</h1>
-        <p>Course description: {course.description}</p>
+        <h1>
+          Course tite:
+          {course.name}
+        </h1>
+        <p>
+          Course description:
+          {course.description}
+        </p>
         <ul>
           <li>
-            <p>Course type: {course.course_type}</p>
+            <p>
+              Course type:
+              {course.course_type}
+            </p>
           </li>
           <li>
-            <p>Course price: {course.price}</p>
+            <p>
+              Course price:
+              {course.price}
+            </p>
           </li>
           <li>
-            <p>Start date: {course.start_date}</p>
+            <p>
+              Start date:
+              {course.start_date}
+            </p>
           </li>
           <li>
-            <p>End date: {course.end_date}</p>
+            <p>
+              End date:
+              {course.end_date}
+            </p>
           </li>
         </ul>
-        <button 
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
-        onClick={() => hanldeEnroll()}>
+        <button
+          type="button"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+          onClick={() => hanldeEnroll()}
+        >
           Enroll Course
         </button>
       </div>
