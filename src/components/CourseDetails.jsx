747ecaa24250075b2c252b9f312293a35524ee3d
biruk-bereka <birukbeletech1212@gmail.com>
2023-06-20 23:19:41 +0300

    Change image source

--- src/components/CourseDetails.jsx
+++ src/components/CourseDetails.jsx
@@ -29,8 +29,8 @@ arrow_back
         <img
           className="course-image"
           width="50%"
-          height="50%"
-          src="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+          heiht="50%"
+          src={course.image_url}
           alt={course.title}
         />
         <div className="flex flex-col md:w-2/5">
