d296e0ad02c7b194af9b27fe5c019d5e2a5db73e
Gustavo Porcilio <gustavoporcilio@gmail.com>
2023-06-30 19:49:34 -0300

    Refactor AddReservation

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -44,7 +44,7 @@ const NavMenu = ({ bgColor, isBacking, isHide }) => {
         </span>
       </button>
 
-      <span className="user">{localStorage.getItem('name')}</span>
+      <span className="user-logued">{localStorage.getItem('name')}</span>
 
       <button
         type="button"
